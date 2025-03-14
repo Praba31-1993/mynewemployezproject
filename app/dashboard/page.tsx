7d11eb9aee9d25e95599ba2e6795d26023d52ef7
Praba31-1993 <prabaidymoto@gmail.com>
2025-03-14 16:19:51 +0530

    Abc

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -3,14 +3,13 @@ export const dynamic = "force-dynamic"; // Keep this
 
 import React from "react";
 import { default as dynamicImport } from "next/dynamic"; // Renamed import
-import SuperAdminDashboard from "./screens/superadmin";
 
 const Sidebar = dynamicImport(() => import("../sidebar/layout"), { ssr: false });
 
 const Dashboard = () => {
   return (
     <Sidebar>
-      <SuperAdminDashboard />
+     <div className="">ABC</div>
     </Sidebar>
   );
 };
