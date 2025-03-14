8d97acf194b8d237608367b0a4282988e8af2863
Praba31-1993 <prabaidymoto@gmail.com>
2025-03-14 19:00:37 +0530

    dashs

--- app/api/axiosInstance.tsx
+++ app/api/axiosInstance.tsx
@@ -1,7 +1,7 @@
 import axios from "axios";
 
-const base_url = "http://192.168.10.126:8080/employez/";
-
+// const base_url = "http://192.168.10.126:8080/employez/";
+const base_url="http://localhost:8080/employez/"
 export const axiosInstance = axios.create({
   baseURL: base_url,
   headers: {
