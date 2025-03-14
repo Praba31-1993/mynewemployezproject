4ce2d2de083fdd9238ebb3ddd20c68869c0717d0
Praba31-1993 <prabaidymoto@gmail.com>
2025-03-14 15:50:33 +0530

    document

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,13 @@
 import type { NextConfig } from "next";
+import path from "path";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  webpack: (config, { isServer }) => {
+    if (!isServer) {
+      config.resolve.fallback = { document: false };
+    }
+    return config;
+  },
 };
 
 export default nextConfig;
